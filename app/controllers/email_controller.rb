4433613cadb89694762dabe6ad2ebf12b669e560
Vasyuk <misha2018@gmail.com>
2017-09-01 17:28:59 +0200

    deleteAjax

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,4 +2,17 @@ class EmailController < ApplicationController
   def index
     @email = Email.all
   end
+
+  def destroy
+     @email = Email.find(params[:id])
+     @email.destroy
+
+     respond_to do |format|
+        format.html { redirect_to ponies_url }
+        format.json { head :no_content }
+        format.js   { render :layout => false }
+     end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'email#index'
 
+  resources :email
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
